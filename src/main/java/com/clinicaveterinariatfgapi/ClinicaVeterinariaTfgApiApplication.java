de5108a8c365d3584d9706c9b8254b343810a7c1
David Poma <davidpoma030@outlook.es>
2023-05-21 15:22:22 +0200

    Initial commit

---
+++ src/main/java/com/clinicaveterinariatfgapi/ClinicaVeterinariaTfgApiApplication.java
@@ -0,0 +1,13 @@
+package com.clinicaveterinariatfgapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaVeterinariaTfgApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaVeterinariaTfgApiApplication.class, args);
+	}
+
+}
