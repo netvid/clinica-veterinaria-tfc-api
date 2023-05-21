6123e56a623233b66452c7cf395eb6f5a7992d8b
David Poma <davidpoma030@outlook.es>
2023-05-21 16:02:06 +0200

    Create dtos done

---
+++ src/main/java/com/clinicaveterinariatfgapi/domain/dto/VeterinarioDTO.java
@@ -0,0 +1,10 @@
+package com.clinicaveterinariatfgapi.domain.dto;
+
+public class VeterinarioDTO {
+    private String dni;
+    private String nombre;
+
+    private String apellidos;
+    private String sector;
+    private String fechaNac;
+}
